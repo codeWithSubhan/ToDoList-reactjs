ea75009b2b9744bf3055181f7f0f659f13e4929b
suvhan <suvhan13@gmail.com>
2025-06-15 18:00:51 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+// import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import BookProvider from "./provider/BookContext";
+// import reportWebVitals from "./reportWebVitals";
+// import BookProvider from "./provider/BookContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BookProvider>
-      <App />
-    </BookProvider>
+    {/* <BookProvider> */}
+    <App />
+    {/* </BookProvider> */}
   </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
